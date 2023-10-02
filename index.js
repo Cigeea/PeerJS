96776a379faf26a5b9d3fad5966d6939593c9bc9
Cigeea <aeegic@live.fr>
2023-10-02 18:33:34 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { PeerServer } = require("peer");
+
+const PORT = 9002;
+
+const peerServer = PeerServer({
+	port: PORT,
+	key: "demodemo",
+	path: "/myapp",
+	allow_discovery: true
+});
+
+peerServer.on("connection", c => {
+	console.log("Connection ID:", c.id);
+})
+
+peerServer.on("disconnect", c => {
+	console.log("disconnect!", c.id);
+})
+
+console.log(`Running Peer JS server on port ${PORT}`);
+//Run by launching node index.js
